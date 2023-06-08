704523bf321ea559534d8f4c5b237fb80a1464b9
John Klink <john.r.klink92@gmail.com>
2023-06-07 20:32:13 -0500

    react deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
@@ -9,6 +9,22 @@ import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://api.github.com/repos/johnrklink/react-portfolio/pages/deployments');
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.log('Error fetching data:', error);
+    }
+  };
+
   return (
     <Router>
       <div className="App">

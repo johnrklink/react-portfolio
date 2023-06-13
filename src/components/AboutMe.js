91c6422e74872315781d42fc0fa4eae4029a385e
John Klink <john.r.klink92@gmail.com>
2023-06-13 18:45:00 -0500

    worked on css adn worked on deployment

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProfilePicture from '../assets/profile.png';
-import '../App.css'
+
+import '../Card.css';
 
 function AboutMe() {
-  return (
-    <div className="section">
-      <h2>About Me</h2>
-      
-      <img
-  className='profile-img'
-  src={ProfilePicture}
-  alt="Profile Picture"
-  style={{maxWidth: "300px", maxHeight: "300px"}}
-/>
+  const [flipped, setFlipped] = useState(false);
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris scelerisque consequat feugiat. Etiam at purus augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In volutpat dignissim risus, id interdum nunc sagittis a. Nulla eget diam quis quam semper facilisis. Fusce a ante dolor. Donec et massa lacus. Vestibulum ultricies enim justo, eu facilisis mi accumsan et. Proin nec velit elit. Etiam gravida leo sed elit varius, vel tincidunt leo consequat.</p>
+  const handleClick = () => {
+    setFlipped(!flipped);
+  };
+
+  return (
+    <div className={`section card ${flipped ? 'flipped' : ''}`} onClick={handleClick}>
+      <div className="card-front">
+        <img
+          className="profile-img"
+          src={ProfilePicture}
+          alt="Profile Picture"
+          style={{ maxWidth: '300px', maxHeight: '300px' }}
+        />
+      </div>
+      <div className="card-back">
+        <h2>About Me</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris scelerisque consequat feugiat. Etiam at purus
+          augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In volutpat
+          dignissim risus, id interdum nunc sagittis a. Nulla eget diam quis quam semper facilisis. Fusce a ante dolor.
+          Donec et massa lacus. Vestibulum ultricies enim justo, eu facilisis mi accumsan et. Proin nec velit elit.
+          Etiam gravida leo sed elit varius, vel tincidunt leo consequat.
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BookHubPic from '../assets/book-hub.png'
 import JobBoardPic from '../assets/job-board.png'
 import PetConnectPic from '../assets/pet-connect.png'
 import '../App.css'
+import '../Card.css'
+
+function Card({ project }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleFlip}>
+      <div className="card-front">
+        <img src={project.image} alt={project.title} className="portfolio-img" />
+        <h3>{project.title}</h3>
+      </div>
+      <div className="card-back">
+        <p>{project.description}</p>
+        <p><a href={project.deployedLink}>View deployed app</a></p>
+        <p><a href={project.githubLink}>View on GitHub</a></p>
+      </div>
+    </div>
+  );
+}
 
 function Portfolio() {
   const projects = [
@@ -22,24 +45,17 @@ function Portfolio() {
     },
     {
       title: 'Pet-Connect',
-      image : PetConnectPic,
+      image: PetConnectPic,
       deployedLink: 'https://project3.com',
       githubLink: 'https://github.com/johnrklink/pet-connect?organization=johnrklink&organization=johnrklink',
     },
-  
   ];
 
   return (
     <section>
       <h2>Portfolio</h2>
       {projects.map(project => (
-        <div key={project.title}>
-          <h3>{project.title}</h3>
-          <img src={project.image} alt={project.title} className="portfolio-img" />
-          <p><a href={project.deployedLink}>View deployed app</a></p>
-          <p><a href={project.githubLink}>View on GitHub</a></p>
-          <p>{project.description}</p>
-        </div>
+        <Card key={project.title} project={project} />
       ))}
     </section>
   );
